d8cba2c6617f5b53c49867e98dc5847de241a09b
Arnaud <bandonkeyea@gmail.com>
2022-08-01 14:09:28 -0600

    Create Student class which inherit in Person

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_accessor :age, :name, :classroom, :parent_permission
+
   def initialize(classroom)
     @classroom = classroom
     super(age, name, parent_permission)
