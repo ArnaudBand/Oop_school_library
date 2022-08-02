3b8fdd4ac49361a35a337d431073b92419770f8d
Arnaud <bandonkeyea@gmail.com>
2022-08-02 14:12:10 -0600

    Create result file to implement the result

---
+++ result.rb
@@ -0,0 +1,10 @@
+require './person'
+require_relative './capital'
+require_relative './trim'
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+capitalizedTrimmedPerson.correct_name
