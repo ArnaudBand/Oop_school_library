80effb0a761e626098ae73dbce946592bdd1689f
Arnaud <bandonkeyea@gmail.com>
2022-08-02 15:27:19 -0600

    Add Nameable class in the decorator file

--- decorator.rb
+++ decorator.rb
@@ -1,4 +1,8 @@
-require './nameable'
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+end
 
 class Decorator < Nameable
   attr_accessor :nameable
