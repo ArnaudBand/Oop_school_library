1021dcacd699559fb05a1842bb4243a9094afdd0
Arnaud <bandonkeyea@gmail.com>
2022-08-03 10:41:59 -0600

    Create book class, initialize "title author" set and get them

--- book.rb
+++ book.rb
@@ -1,12 +1,7 @@
 class Book
   attr_accessor :title, :author
-
   def initialize(title, author)
     @title = title
     @author = author
-  end
-
-  def add_rental(person, date)
-    Rental.new(date, self, person)
   end
 end
