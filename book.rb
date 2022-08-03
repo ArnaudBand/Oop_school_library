e6fcabcd77a8c0a784c67ba4eb3542f6159cc4a5
Arnaud <bandonkeyea@gmail.com>
2022-08-03 10:50:46 -0600

    Create book has many rentals

--- book.rb
+++ book.rb
@@ -1,7 +1,16 @@
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(person, date)
+    Rental.new(date, self, person)
+  end
+
+  def save_rental(rental)
+    @rentals << rental unless @rentals.include?(rental)
   end
 end
