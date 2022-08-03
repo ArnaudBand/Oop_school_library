7a5f61971900d733d680d2857449ec5123130e06
Arnaud <bandonkeyea@gmail.com>
2022-08-03 10:37:40 -0600

    Create Classroom has many students

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,14 @@
 class Classroom
   attr_accessor :label
-  def initialize(label)
+  attr_reader :students
+
+  def initialize(label, students = [])
     @label = label
+    @students = students
+  end
+
+  def add_student(student)
+    @students << student unless @students.include?(student)
+    student.classroom = self
   end
 end
