42eb526eb69723f99f4b14b002eb06da9d01a594
Arnaud <bandonkeyea@gmail.com>
2022-08-05 17:44:14 -0600

    Add menu to choose options needed

--- main.rb
+++ main.rb
@@ -18,4 +18,21 @@ class Main
     7. Exit
     Please choose an option:'
   end
+
+  def menu
+    loop do
+      puts menu_option
+      option = gets.chomp.to_i
+      case option
+      when 1 then @app.list_books
+      when 2 then @app.list_people
+      when 3 then @app.create_person
+      when 4 then @app.create_book
+      when 5 then @app.create_rental
+      when 6 then @app.list_rental
+      when 7 then exit
+      else puts 'Invalid option'
+      end
+    end
+  end
 end
