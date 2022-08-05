e5dcd3eebd479abed15cec537ab3119bbf20a2ec
Arnaud <bandonkeyea@gmail.com>
2022-08-05 17:43:15 -0600

    Add menu option

--- main.rb
+++ main.rb
@@ -6,4 +6,16 @@ class Main
   def initialize
     @app = App.new
   end
+
+  def menu_option
+    'Do you want to choose an option in the menu? Please type an option number: '
+    1. List books
+    2. List people
+    3. Create person
+    4. Create book
+    5. Create rental
+    6. List rentals
+    7. Exit
+    Please choose an option:'
+  end
 end
