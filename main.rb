9450f4e3f6df8871618137eb2082d3983fbc02a9
Arnaud <bandonkeyea@gmail.com>
2022-08-08 13:12:24 -0600

    Add the input validation, use require_relative

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require './app'
+require_relative './app'
 
 class Main
   attr_reader :app
@@ -7,8 +7,42 @@ class Main
     @app = App.new
   end
 
+  def menu_option
+    'Do you want to choose an option in the menu? Please type an option number:
+    1. List books
+    2. List people
+    3. Create person
+    4. Create book
+    5. Create rental
+    6. List rentals
+    7. Exit
+    Please choose an option:'
+  end
+
+  def menu # rubocop:disable Metrics/CyclomaticComplexity
+    loop do
+      option = gets.chomp.to_i
+      case option
+      when 1 then @app.list_books
+      when 2 then @app.list_people
+      when 3 then @app.create_person
+      when 4 then @app.create_book
+      when 5 then @app.create_rental
+      when 6 then @app.list_rental
+      when 7 then exit
+      else puts 'Invalid option'
+      end
+    end
+  end
+
   def main
-    @app.run
+    puts 'Welcome to the library'
+    puts '======================'
+    puts menu_option
+    puts '***********'
+    menu
+    puts 'Thank you for using the library'
+    puts '=============================='
   end
 end
 
