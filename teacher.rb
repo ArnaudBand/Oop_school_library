56ab4dd1821e35367b8558f16f9ee7bff351c802
Arnaud <bandonkeyea@gmail.com>
2022-08-09 15:28:10 -0600

    Add id to initialize method, and 2 method to_json, json_create method to create an object

--- teacher.rb
+++ teacher.rb
@@ -3,12 +3,24 @@ require './person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(name, age, specialization)
+  def initialize(name, age, specialization, id = Random.rand(1..999))
     @specialization = specialization
-    super(age, name)
+    super(age, name, id)
   end
 
   def can_use_services?
     true
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'data' => [name, age, specialization, id]
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    name, age, id, specialization = object['data']
+    new(name, age, id, specialization)
   end
 end
